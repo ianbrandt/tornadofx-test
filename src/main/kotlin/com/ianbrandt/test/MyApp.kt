a75781cb74ab50052dcc3d0cd76f0679754dc3ea
Ian Brandt <ian@ianbrandt.com>
2018-06-03 12:16:50 -0700

    Fixed an issue where TornadoFX requires `import tornadofx.*` for its DSL to resolve properly.

--- src/main/kotlin/com/ianbrandt/test/MyApp.kt
+++ src/main/kotlin/com/ianbrandt/test/MyApp.kt
@@ -1,21 +1,15 @@
 package com.ianbrandt.test
 
-import javafx.application.Application
-import javafx.scene.control.Button
-import javafx.scene.layout.VBox
-import tornadofx.App
-import tornadofx.View
+import tornadofx.*
 
 fun main(args: Array<String>) {
-	Application.launch(MyApp::class.java, *args)
+	launch<MyApp>(args)
 }
 
 class MyApp : App(MyView::class)
 
 class MyView : View() {
-	override val root = VBox()
-
-	init {
-		root += Button("Press Me")
+	override val root = vbox {
+		label("Hello, World!")
 	}
 }
