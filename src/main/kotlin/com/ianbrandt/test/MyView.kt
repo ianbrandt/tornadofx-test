316d2abd5ed721ce5b21eaed56eafb231e9e9428
Ian Brandt <ian@ianbrandt.com>
2019-05-26 11:50:38 -0700

    Improved test case to factor out stdout.

--- src/main/kotlin/com/ianbrandt/test/MyView.kt
+++ src/main/kotlin/com/ianbrandt/test/MyView.kt
@@ -3,11 +3,16 @@ package com.ianbrandt.test
 import tornadofx.*
 
 class MyView : View() {
+	companion object {
+		const val BUTTON_ID = "test-button"
+		const val INITIAL_BUTTON_TEXT = "Test"
+		const val CHANGED_BUTTON_TEXT = "Success"
+	}
 	override val root = vbox {
-		button("Test") {
-			id = "test-button"
+		button(INITIAL_BUTTON_TEXT) {
+			id = BUTTON_ID
 			action {
-				println("TEST ACTION")
+				text = CHANGED_BUTTON_TEXT
 			}
 		}
 	}
--- src/test/kotlin/com/ianbrandt/test/MyViewTest.kt
+++ src/test/kotlin/com/ianbrandt/test/MyViewTest.kt
@@ -1,10 +1,13 @@
 package com.ianbrandt.test
 
 import javafx.scene.Scene
+import javafx.scene.control.Button
 import javafx.stage.Stage
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.testfx.api.FxRobot
+import org.testfx.assertions.api.Assertions
+import org.testfx.assertions.api.Assertions.*
 import org.testfx.framework.junit5.ApplicationExtension
 import org.testfx.framework.junit5.Start
 
@@ -18,8 +21,11 @@ class MyViewTest {
 	}
 
 	@Test
-	fun `test the button`(robot: FxRobot) {
-		println("TESTING...")
-		robot.clickOn("#test-button")
+	fun `test button text changes`(robot: FxRobot) {
+		val buttonQuery = "#${MyView.BUTTON_ID}"
+		val button = robot.lookup(buttonQuery).queryButton()
+		assertThat(button).hasText(MyView.INITIAL_BUTTON_TEXT)
+		robot.clickOn(button)
+		assertThat(button).hasText(MyView.CHANGED_BUTTON_TEXT)
 	}
 }
